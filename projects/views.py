40ce9afaebbaceab833eac9400e883e2faf5c034
hecmandark <{email}>
2025-05-26 19:27:06 +0200

    Sort projects by date in the projects view

--- projects/views.py
+++ projects/views.py
@@ -3,5 +3,5 @@ from .models import Project
 
 
 def projects(request):
-    projects=Project.objects.all()
+    projects=Project.objects.all().order_by('-date')
     return render(request,'index.html',{"projects":projects})
